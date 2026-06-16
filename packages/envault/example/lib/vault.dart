2a4d400e9b574f266dea226e8d6d631ed5af6b43
Gunjan Sharma <gunjan@altdrx.com>
2026-06-16 19:43:12 +0530

    chore: add example directory to envault package and shorten description for pub points

---
+++ packages/envault/example/lib/vault.dart
@@ -0,0 +1,17 @@
+import 'package:envault/envault.dart';
+
+part 'vault.g.dart';
+
+@VaultEnv(
+  path: '.env.production',
+  obfuscation: ObfuscationLevel.aesGcm256,
+  gitIgnoreCheck: GitIgnoreCheck.failBuild,
+  strictMode: true,
+)
+abstract class Vault {
+  @VaultField(
+    varName: 'API_KEY',
+    minEntropyBits: 128,
+  )
+  static SecureString get apiKey => _Vault.apiKey;
+}
